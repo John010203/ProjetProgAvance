bdcf7d1c639683ec01a7c3eb0951ca5efd26700b
Jonathan <98401536+John010203@users.noreply.github.com>
2023-10-16 19:30:43 +0200

    ajout menus

--- Agglomeration.java
+++ Agglomeration.java
@@ -5,18 +5,32 @@ public class Agglomeration {
 	private List <Ville> villes;
 	private List<Route> routes;
 	
-	public Agglomeration(List<Ville> Villes) {
-		this.villes = new ArrayList<Ville>();
-		this.routes = null;
+	public Agglomeration(List<Ville> villes) {
+		this.villes = new ArrayList<Ville>(villes);
+		this.routes = new ArrayList<Route>();
 	}
 	
 	public String getVilles() {
 		StringBuffer sb = new StringBuffer();
 		for (int i =0; i< villes.size();i++) {
 			sb.append(villes.get(i).getNomVille());
+			sb.append(" ");
 		}
-		
 		return sb.toString();
 	}
+	
+	public void setRoute(Route route) {
+		routes.add(route);
+	}
+	
+	public void recharge(String ville) {
+		boolean rep = true;
+		int i = 0;
+		while(rep) {
+			if(this.villes.get(i).getNomVille() == ville) {
+				
+				rep = false;
+			}
+		}
+	}
 }
-
--- TestAgglomeration.java
+++ TestAgglomeration.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TestAgglomeration {
 	public static void main(String[] args) {
@@ -9,7 +10,52 @@ public class TestAgglomeration {
 		a.add(v);
 		a.add(v1);
 		Agglomeration Ag = new Agglomeration(a);
-		System.out.println(v.getNomVille());
 		System.out.println(Ag.getVilles());
+		
+		Scanner s = new Scanner(System.in);
+		int i, reponse;
+		String borne;
+		do {
+			System.out.println("Entrez le nombre de villes (entre 1 et 26 inclus) : ");
+			i = s.nextInt();
+		}
+		while(i>26 && i < 0);
+		System.out.println("L'agglomération à " + i + " villes");
+		
+		boolean rep=true;
+		while(rep) {
+			System.out.println("1. Ajouter une route.");
+			System.out.println("2. fin.");
+			reponse = s.nextInt();
+			switch(reponse) {
+			case 1:
+				Ag.setRoute(new Route(v,v1));
+				break;
+			case 2:
+				rep=false;
+				break;
+			}
+		}
+		
+		rep=true;
+		while(rep) {
+			System.out.println("1. Ajouter une borne de recharge.");
+			System.out.println("2. Retirer une borne de recharge.");
+			System.out.println("3. fin.");
+			reponse = s.nextInt();
+			switch(reponse) {
+			case 1:
+				System.out.println("Dans quelle ville souhaitez-vous ajouter une borne de recharge? ");
+				borne = s.nextLine();
+				v.setZoneDeRecharge(true);
+				break;
+			case 2:
+				v.setZoneDeRecharge(false);
+				break;
+			case 3:
+				rep=false;
+				break;
+			}
+		}
 	}
 }
